2b99db2bbe74d24497888892c03c58a7fea15759
Daniel-Kao <396007476@qq.com>
2020-01-12 22:36:52 +0800

    dockerize

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,26 @@
 import express from "express";
+import Redis from 'ioredis'
+import session from 'express-session'
+import connectRedis from 'connect-redis'
+
+const RedisStore = connectRedis(session)
+
+const client = new Redis({
+  port
+  host
+})
 
 const app = express();
+
+app.use(
+  session({
+    store: new RedisStore({ client }),
+    secret: 'keyboard cat',
+    resave: false,
+  })
+)
+
+
 
 app.get("/", (req, res) => {
   res.json({ message: "hello" });
